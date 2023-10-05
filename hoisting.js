b3acdcff2849686b5de048c7f452ce270cad9f74
amitrajpoot99 <amitrajpoot99@gmail.com>
2023-10-05 21:16:50 +0530

    commit

---
+++ hoisting.js
@@ -0,0 +1,9 @@
+ var func
+
+ func=50
+
+ function func(){
+    console.log('func')
+ }
+
+func()
---
+++ lengthOfFunction.js
@@ -0,0 +1,20 @@
+const f1=()=>{
+
+}
+console.log(f1.length)
+
+const f2=(a,b)=>{
+
+}
+console.log(f2.length)
+
+
+const f3 = (a,b,c,d) => {
+
+}
+console.log(f3.length)
+
+const f4 = (a,b=1,c) => {
+
+}
+console.log(f4.length)
